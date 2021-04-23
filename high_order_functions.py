ece1d78cf1506d315c7e29bcccd76db123ebcb6c
JLob Lap <joselobatof@gmail.com>
2021-04-23 13:37:58 -0500

    filter function

--- high_order_functions.py
+++ high_order_functions.py
@@ -4,8 +4,13 @@ def run():
     saludo = lambda func: func()
 
     saludo(hola)
-    saludo(adios)
 
+    numbers = [ i for i in range(1,50)]
+    odds = list(filter(lambda x: x % 2 != 0, numbers))
+
+    print(odds)
+
+    saludo(adios)
 
 if __name__ == '__main__':
     run()
