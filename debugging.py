478021766af48c2712e4c6ba26cca1b95eb5efa3
JLob Lap <joselobatof@gmail.com>
2021-04-23 18:48:07 -0500

    add raise to valueError

--- debugging.py
+++ debugging.py
@@ -9,10 +9,12 @@ def divisors(num):
 def run():
     try:
         num = int(input('Type a number: '))
+        if(num <= 0):
+            raise ValueError
         print(divisors(num))
         print('fin')
     except ValueError as ve:
-        print("It should be a number")
+        print("It should be a positive integer number")
 
 if __name__ == '__main__':
     run()
