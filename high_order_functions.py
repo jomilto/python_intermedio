850f74abcf432e00b34840feab0b93c58a7d4e6f
JLob Lap <joselobatof@gmail.com>
2021-04-23 13:43:31 -0500

    reduce function

--- high_order_functions.py
+++ high_order_functions.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 def run():
     hola = lambda: print('Hola')
     adios = lambda: print('Adios')
@@ -5,13 +7,16 @@ def run():
 
     saludo(hola)
 
-    numbers = [ i for i in range(1,50)]
+    numbers = [ i for i in range(1,15)]
     odds = list(filter(lambda x: x % 2 != 0, numbers))
 
     squares = list(map(lambda x: x**2,numbers))
 
+    all_multiplied = reduce(lambda a,b: a*b, numbers)
+
     print(odds)
     print(squares)
+    print(all_multiplied)
 
     saludo(adios)
 
